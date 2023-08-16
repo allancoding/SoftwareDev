7d1e1cb1be4b34924c520d5832695fd296e66143
Allan Niles <60013695a@gmail.com>
2023-08-16 15:11:01 -0600

    Add files via upload

---
+++ start.ps1
@@ -0,0 +1,8 @@
+winget install Microsoft.VisualStudioCode
+
+cd "C:\Users\student\Documents"
+git clone https://github.com/allancoding/SoftwareDev
+git config user.name "Allan Niles"
+git config user.email "60013695a@gmail.com"
+
+code "C:\Users\student\Documents\SoftwareDev"
