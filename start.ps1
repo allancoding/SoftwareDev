fdc8f0f41c556fb3f5466fcb0d9e8191a90e3c95
Allan Niles <60013695a@gmail.com>
2023-08-17 10:58:46 -0600

    add --global

--- start.ps1
+++ start.ps1
@@ -2,7 +2,7 @@ winget install Microsoft.VisualStudioCode
 
 cd "C:\Users\student\Documents"
 git clone https://github.com/allancoding/SoftwareDev
-git config user.name "Allan Niles"
-git config user.email "60013695a@gmail.com"
+git config --global user.name "Allan Niles"
+git config --global user.email "60013695a@gmail.com"
 
 code "C:/Users/student/Documents/SoftwareDev"
