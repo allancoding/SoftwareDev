3b14544a36ea17f0b8bea6d4d9445ba03dec1970
Allan Niles <allancoding.dev@gmail.com>
2023-09-19 10:39:01 -0600

    9/19/2023

---
+++ src/Course_3/Exercise_44/Exercise_44_03.java
@@ -0,0 +1,20 @@
+package Course_3.Exercise_44;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Exercise_44_03 {
+
+  @Test
+  public void testIsPalindrome() {
+    assertTrue(Course_2.Exercise_06.Exercise_06_3.isPalindrome(12321));
+    assertFalse(Course_2.Exercise_06.Exercise_06_3.isPalindrome(123));
+    assertTrue(Course_2.Exercise_06.Exercise_06_3.isPalindrome(1));
+  }
+
+  @Test
+  public void testReverse() {
+    assertEquals(Course_2.Exercise_06.Exercise_06_3.reverse(123), 321);
+    assertEquals(Course_2.Exercise_06.Exercise_06_3.reverse(12321), 12321);
+    assertEquals(Course_2.Exercise_06.Exercise_06_3.reverse(1), 1);
+  }
+}
---
+++ src/Course_3/Exercise_44/Exercise_44_37.java
@@ -0,0 +1,13 @@
+package Course_3.Exercise_44;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Exercise_44_37 {
+
+    @Test
+    public void testFormat() {
+        assertEquals(Course_2.Exercise_06.Exercise_06_37.format(123, 5), "00123");
+        assertEquals(Course_2.Exercise_06.Exercise_06_37.format(123, 2), "123");
+        assertEquals(Course_2.Exercise_06.Exercise_06_37.format(1, 5), "00001");
+    }
+}
