719a2e9dea6d14f8fc99f749d1c5f226a6e4026f
Allan Niles <allancoding.dev@gmail.com>
2023-09-21 10:26:49 -0600

    9/21/2023

---
+++ src/Tests/JavaDocTest.java
@@ -0,0 +1,39 @@
+package Tests;
+/**
+ * <h1>JavaDoc Test</h1>
+ * <p>This is the description of the class</p>
+ *
+ * <a href="https://goo.gl/fr8PHb">Click here for more information on JavaDocs</a>
+ *
+ * <p>Created: date goes here</p>
+ * 
+ * @author my name goes here
+*/
+public class JavaDocTest {
+	/**
+	 * This is the main method which makes use of......
+	 * 
+	 * @param args (String[]; unused)
+	*/
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * This method is used to......
+	 * 
+	 * <pre>Examples:
+	 * {@code 	returnType(4.2, 6) returns 6
+	 * 	returnType(3, 8) returns 6
+	 * 
+	 * }</pre>
+	 * 
+	 * @param d (double; what it represents)
+	 * @param i (int; what it represents)
+	 * @return val (int; what it represents)
+	*/
+	public static int returnType(double d, int i) {
+		int val = 6;
+		return val;
+	}
+}
--- start.ps1
+++ start.ps1
@@ -1,3 +1,4 @@
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name AppsUseLightTheme -Value 0 -PropertyType DWord -Force
 Get-Process chrome | ForEach-Object { $_.CloseMainWindow() | Out-Null}
 start chrome -ArgumentList '--new-window https://stech.instructure.com/login/canvas https://discord.com/app https://github.com/login'
 winget install Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders,addtopath"'
@@ -6,5 +7,7 @@ git clone https://github.com/allancoding/SoftwareDev
 git config --global user.name "Allan Niles"
 git config --global user.email "allancoding.dev@gmail.com"
 Set-Location "C:\Users\student\Documents\SoftwareDev"
-#Set-Variable classpath=.;%classpath%;C:\Users\student\Documents\SoftwareDev\lib\junit.jar
-Start-Process -NoNewWindow -FilePath "C:\Users\student\AppData\Local\Programs\Microsoft VS Code\Code.exe" -ArgumentList "."
+start code .
+Start-Sleep 1
+Get-CimInstance win32_process -Filter "Name like 'conhost.exe'" | ? { (Get-Process -id $_.ParentProcessId -ea Ignore) -eq $null } | Select-Object ProcessId | ? { Stop-Process $_.ProcessId -Force }
+exit
