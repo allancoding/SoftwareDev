22096964db10a3c5e01b14e280fc8fb4e2817b63
Allan Niles <allancoding.dev@gmail.com>
2023-10-31 08:45:32 -0600

    10/31/2023

--- src/Course_4/Exercise_09/Exercise_09_02.java
+++ src/Course_4/Exercise_09/Exercise_09_02.java
@@ -1,5 +1,22 @@
 package Course_4.Exercise_09;
 
+public class Exercise_09_02 {
+
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle(4, 40);
+        Rectangle rectangle2 = new Rectangle(3.5, 35.9);
+        System.out.println("Rectangle 1:");
+        System.out.println("Width: " + rectangle1.width);
+        System.out.println("Height: " + rectangle1.height);
+        System.out.println("Area: " + rectangle1.getArea());
+        System.out.println("Perimeter: " + rectangle1.getPerimeter());
+        System.out.println("Rectangle 2:");
+        System.out.println("Width: " + rectangle2.width);
+        System.out.println("Height: " + rectangle2.height);
+        System.out.println("Area: " + rectangle2.getArea());
+        System.out.println("Perimeter: " + rectangle2.getPerimeter());
+    }
+}
 class Rectangle {
     double width = 1;
     double height = 1;
