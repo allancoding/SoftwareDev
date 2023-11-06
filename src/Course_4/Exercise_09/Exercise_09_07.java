276e4a298de88e1f61b5d0bfb71fa909d00e395c
Allan Niles <allancoding.dev@gmail.com>
2023-11-06 09:51:33 -0700

    11/6/2023

--- src/Course_4/Exercise_09/Exercise_09_07.java
+++ src/Course_4/Exercise_09/Exercise_09_07.java
@@ -1,8 +1,6 @@
 package Course_4.Exercise_09;
-import java.util.Date;
 
 public class Exercise_09_07 {
-
     public static void main(String[] args) {
         Account account = new Account(1122, 20000);
         account.setAnnualInterestRate(4.5);
@@ -11,53 +9,5 @@ public class Exercise_09_07 {
         System.out.println("Balance: " + account.getBalance());
         System.out.println("Monthly interest: " + account.getMonthlyInterest());
         System.out.println("Date created: " + account.getDateCreated());
-    }
-}
-
-class Account {
-    private int id = 0;
-    private double balance = 0;
-    private double annualInterestRate = 0;
-    private Date dateCreated;
-    Account() {
-        dateCreated = new Date();
-    }
-    Account(int newId, double newBalance) {
-        id = newId;
-        balance = newBalance;
-        dateCreated = new Date();
-    }
-    int getId() {
-        return id;
-    }
-    void setId(int newId) {
-        id = newId;
-    }
-    double getBalance() {
-        return balance;
-    }
-    void setBalance(double newBalance) {
-        balance = newBalance;
-    }
-    double getAnnualInterestRate() {
-        return annualInterestRate;
-    }
-    void setAnnualInterestRate(double newAnnualInterestRate) {
-        annualInterestRate = newAnnualInterestRate;
-    }
-    Date getDateCreated() {
-        return dateCreated;
-    }
-    double getMonthlyInterestRate() {
-        return annualInterestRate / 12;
-    }
-    double getMonthlyInterest() {
-        return balance * getMonthlyInterestRate() / 100;
-    }
-    void withdraw(double amount) {
-        balance -= amount;
-    }
-    void deposit(double amount) {
-        balance += amount;
     }
 }
