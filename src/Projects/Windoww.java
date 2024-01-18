f922e713ef0d53dd2b7e0e41844a4b64d0b0abf1
Allan Niles <allancoding.dev@gmail.com>
2024-01-18 08:40:35 -0700

    move a file

--- src/Projects/Windoww.java
+++ src/Projects/Windoww.java
@@ -2,13 +2,8 @@ package Projects;
 
 import java.io.IOException;
 import javax.swing.*;
-import javax.imageio.ImageIO;
-import java.net.URL;
-import java.util.Date;
-import java.io.File;
 
 public class Windoww extends JFrame{
-    private static final String DEFAULT_IMAGE_PATH = "./cat.gif";
     public Windoww() {
         super("Windowwwww!");
         int width = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
@@ -16,11 +11,9 @@ public class Windoww extends JFrame{
         int x = (int) (Math.random() * (width - 300));
         int y = (int) (Math.random() * (height - 150));
         setLocation(x, y);
-        setSize(300, 300);
+        setSize(300, 150);
         setVisible(true);
-        long timestamp = new Date().getTime();
-        JLabel label = new JLabel();
-        loadImageIconFromURLAsync("https://cataas.com/cat?" + timestamp, label);
+        JLabel label = new JLabel("Don't close this window!");
         label.setHorizontalAlignment(JLabel.CENTER);
         getContentPane().add(label);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -30,38 +23,27 @@ public class Windoww extends JFrame{
                 new Windoww();
             }
         });
-    }
-
-    private void loadImageIconFromURLAsync(String imageUrl, JLabel label) {
-        SwingWorker<ImageIcon, Void> worker = new SwingWorker<ImageIcon, Void>() {
+        //check if process is killed my task manager
+        new Thread(new Runnable() {
             @Override
-            protected ImageIcon doInBackground() throws Exception {
-                try {
-                    URL url = new URL(imageUrl);
-                    return new ImageIcon(ImageIO.read(url));
-                } catch (IOException e) {
-                    // If there is an error loading the image, return the default image
-                    return new ImageIcon(ImageIO.read(new File(DEFAULT_IMAGE_PATH)));
+            public void run() {
+                while (true) {
+                    if (!java.awt.Window.getWindows()[0].isShowing()) {
+                        new Windoww();
+                        new Windoww();
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
                 }
             }
-
-            @Override
-            protected void done() {
-                try {
-                    ImageIcon imageIcon = get();
-                    label.setIcon(imageIcon);
-                    revalidate();
-                    repaint();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        worker.execute();
+        }).start();
     }
+
     public static void main(String[] args) throws IOException {
-        JavaW.run("Projects.Windoww");
+        JavaW.run("Windoww");
         new Windoww();
     }
 }
