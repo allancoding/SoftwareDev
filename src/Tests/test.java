e6ffb4154b31eb50954670adfe8a89300018e1f6
Allan Niles <allancoding.dev@gmail.com>
2024-01-30 09:39:37 -0700

    done with test

--- src/Tests/test.java
+++ src/Tests/test.java
@@ -2,8 +2,6 @@ package Tests;
 
 public class test {
 	public static void main(String[] args) {
-		String s1 = "Welcome to Java";
-		String s2 = "Welcome to Java";
-		System.out.println("s1 == s2 is " + s1 == s2);
+		System.gc();
 	}
 }
