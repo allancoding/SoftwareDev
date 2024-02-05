50072bfd4bf070466660684595ec9e568ec38237
Allan Niles <allancoding.dev@gmail.com>
2024-02-05 10:09:12 -0700

    Exercise 12-15

---
+++ src/Course_4/Exercise_12/Exercise_12_15.java
@@ -0,0 +1,32 @@
+package Course_4.Exercise_12;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise_12_15 {
+    public static void main(String[] args) {
+        try {
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream("Exercise12_15.txt"), StandardCharsets.UTF_8));
+            for (int i = 0; i < 100; i++) {
+                writer.print((int) (Math.random() * 100)+ " ");
+            }
+            writer.close();
+            File file = new File("Exercise12_15.txt");
+            Scanner input = new Scanner(file);
+            int[] array = new int[100];
+            for (int i = 0; i < 100; i++) {
+                array[i] = input.nextInt();
+            }
+            input.close();
+            Arrays.sort(array);
+            System.out.println(Arrays.toString(array));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
