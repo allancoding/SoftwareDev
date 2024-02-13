5fd83299190b0c0586f107a7715e933857ee0482
Allan Niles <allancoding.dev@gmail.com>
2024-02-13 09:57:38 -0700

    Extending Classes - Real World

---
+++ src/Course_4/Chapter_13/Extending_Classes.java
@@ -0,0 +1,26 @@
+package Course_4.Chapter_13;
+
+import java.util.Scanner;
+
+public class Extending_Classes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter side 1: ");
+        double side1 = input.nextDouble();
+        System.out.print("Enter side 2: ");
+        double side2 = input.nextDouble();
+        System.out.print("Enter side 3: ");
+        double side3 = input.nextDouble();
+        System.out.print("Enter a color: ");
+        String color = input.next();
+        System.out.print("Is the triangle filled? (y/n): ");
+        boolean filled = input.next().charAt(0) == 'y';
+        Triangle triangle = new Triangle(side1, side2, side3, color, filled);
+        System.out.println(triangle.toString());
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+        System.out.println("Color: " + triangle.getColor());
+        System.out.println("Filled: " + triangle.isFilled());
+        input.close();
+    }
+}
