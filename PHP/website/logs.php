73a630e31a920f62c8b7d20a9db4716672b2dfb4
Allan Niles <allancoding.dev@gmail.com>
2025-01-30 10:02:38 -0700

    donw with 3

--- PHP/website/logs.php
+++ PHP/website/logs.php
@@ -1,11 +1,8 @@
 <?php
-    // Read the file contents
     $logContents = file_get_contents('login_attempts.txt');
     
-    // Split the contents into an array by new lines
     $logArray = explode("\n", $logContents);
     
-    // Sort the array
     sort($logArray);
 ?>
 <html>
