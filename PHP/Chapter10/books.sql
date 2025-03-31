4713f391ef3fc00328579e4579fd59f0b1aee760
Allan Niles <allancoding.dev@gmail.com>
2025-03-31 09:48:11 -0600

    Add 10

---
+++ PHP/Chapter10/books.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 31, 2025 at 03:42 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `books`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `ISBN` char(13) NOT NULL,
+  `Author` char(50) DEFAULT NULL,
+  `Title` char(100) DEFAULT NULL,
+  `Price` float(4,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`ISBN`, `Author`, `Title`, `Price`) VALUES
+('0-672-31509-2', 'Pruitt, et al.', 'Teach Yourself GIMP in 24 Hours', 24.99),
+('0-672-31697-8', 'Michael Morgan', 'Java 2 for Professional Developers', 34.99),
+('0-672-31745-1', 'Thomas Down', 'Installing Debian GNU/Linux', 24.99),
+('0-672-31769-9', 'Thomas Schenk', 'Caldera OpenLinux System Administration Unleashed', 49.99);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_reviews`
+--
+
+CREATE TABLE `book_reviews` (
+  `ISBN` char(13) NOT NULL,
+  `Review` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `book_reviews`
+--
+
+INSERT INTO `book_reviews` (`ISBN`, `Review`) VALUES
+('0-672-31697-8', 'The Morgan book is clearly written and goes well beyond\n                     most of the basic Java books out there.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `CustomerID` int(10) UNSIGNED NOT NULL,
+  `Name` char(50) NOT NULL,
+  `Address` char(100) NOT NULL,
+  `City` char(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`CustomerID`, `Name`, `Address`, `City`) VALUES
+(1, 'Julie Smith', '25 Oak Street', 'Airport West'),
+(2, 'Alan Wong', '1/47 Haines Avenue', 'Box Hill'),
+(3, 'Michelle Arthur', '357 North Road', 'Yarraville');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(10) UNSIGNED NOT NULL,
+  `CustomerID` int(10) UNSIGNED NOT NULL,
+  `Amount` float(6,2) DEFAULT NULL,
+  `Date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`OrderID`, `CustomerID`, `Amount`, `Date`) VALUES
+(1, 3, 69.98, '2007-04-02'),
+(2, 1, 49.99, '2007-04-15'),
+(3, 2, 74.98, '2007-04-19'),
+(4, 3, 24.99, '2007-05-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `OrderID` int(10) UNSIGNED NOT NULL,
+  `ISBN` char(13) NOT NULL,
+  `Quantity` tinyint(3) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`OrderID`, `ISBN`, `Quantity`) VALUES
+(1, '0-672-31697-8', 2),
+(2, '0-672-31769-9', 1),
+(3, '0-672-31509-2', 1),
+(3, '0-672-31769-9', 1),
+(4, '0-672-31745-1', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`ISBN`);
+
+--
+-- Indexes for table `book_reviews`
+--
+ALTER TABLE `book_reviews`
+  ADD PRIMARY KEY (`ISBN`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `CustomerID` (`CustomerID`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`OrderID`,`ISBN`),
+  ADD KEY `ISBN` (`ISBN`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `CustomerID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book_reviews`
+--
+ALTER TABLE `book_reviews`
+  ADD CONSTRAINT `book_reviews_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `books` (`ISBN`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`);
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`),
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`ISBN`) REFERENCES `books` (`ISBN`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
