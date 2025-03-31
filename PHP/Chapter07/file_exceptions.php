f95e7814cdc7962febcd687ac0139ed2937a0c6d
Allan Niles <allancoding.dev@gmail.com>
2025-03-31 09:30:06 -0600

    Update 7

--- PHP/Chapter07/file_exceptions.php
+++ PHP/Chapter07/file_exceptions.php
@@ -1,36 +1,30 @@
 <?php
 
-class fileOpenException extends Exception
-{
-  function __toString()
-  {
-       return "fileOpenException ". $this->getCode()
-              . ": ". $this->getMessage()."<br />"." in "
-              . $this->getFile(). " on line ". $this->getLine()
-              . "<br />";
-   }
-}
+class fileOpenException extends Exception{
+    function __toString(){
+        return "fileOpenException " .$this->getCode().
+                ": " .$this->getMessage()."<br/>" . " in "
+                .$this->getFile(). " on line " .$this->getLine().
+                "<br/>";
+    }
+} // End of class fileOpenException extends Exception
 
-class fileWriteException extends Exception
-{
-  function __toString()
-  {
-       return "fileWriteException ". $this->getCode()
-              . ": ". $this->getMessage()."<br />"." in "
-              . $this->getFile(). " on line ". $this->getLine()
-              . "<br />";
-   }
-}
+class fileWriteException extends Exception{
+    function __toString(){
+        return "fileWriteException " .$this->getCode().
+        ": " .$this->getMessage()."<br/>" . " in "
+            .$this->getFile(). " on line " .$this->getLine().
+            "<br/>";
+    }
+} // End of class fileWriteException extends Exception
 
-class fileLockException extends Exception
-{
-  function __toString()
-  {
-       return "fileLockException ". $this->getCode()
-              . ": ". $this->getMessage()."<br />"." in "
-              . $this->getFile(). " on line ". $this->getLine()
-              . "<br />";
-   }
-}
+class fileLockException extends Exception{
+    function __toString(){
+        return "fileLockException " .$this->getCode().
+        ": " .$this->getMessage()."<br/>" . " in "
+            .$this->getFile(). " on line " .$this->getLine().
+            "<br/>";
+    }
+} // End of class fileLockException extends Exception
 
 ?>
--- PHP/Chapter07/processorder.php
+++ PHP/Chapter07/processorder.php
@@ -1,96 +1,145 @@
 <?php
-  require_once("file_exceptions.php");
+require_once 'file_exceptions.php';
 
-  // create short variable names
-  $tireqty = (int) $_POST['tireqty'];                                           
-  $oilqty = (int) $_POST['oilqty'];                                             
-  $sparkqty = (int) $_POST['sparkqty'];                                         
-  $address = preg_replace('/\t|\R/',' ',$_POST['address']);                     
-  $document_root = $_SERVER['DOCUMENT_ROOT'];                                   
-  $date = date('H:i, jS F Y'); 
+// constant variables
+define("TIREPRICE", 100);
+define("OILPRICE", 10);
+define("SPARKPRICE", 4);
+
+//create short variable names
+# This variables are converted into Integer from String of $_POST[] array.
+$tireqty = (int) $_POST['tireqty'];
+$oilqty = (int) $_POST['oilqty'];
+$sparkqty = (int) $_POST['sparkqty'];
+
+$find = $_POST['find'];
+$address = preg_replace('/\t|\R/', ' ', $_POST['address']);
+$document_root = $_SERVER['DOCUMENT_ROOT'];
+$date = date('H:i, jS F Y');
 ?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Bob's Auto Parts - Order Results</title>
-  </head>
-  <body>
-    <h1>Bob's Auto Parts</h1>
-    <h2>Order Results</h2> 
-    <?php
-      echo "<p>Order processed at ".date('H:i, jS F Y')."</p>";
-      echo "<p>Your order is as follows: </p>";
+    <head>
+    	<title>Bob's Auto Parts - Order Results</title>
+    </head>
+    <body>
+    	<h1>Bob's Auto Parts</h1>
+    	<?php
+       	$totalqty = 0;
+    	$totalqty = $tireqty + $oilqty + $sparkqty;
+    	
+    	if($totalqty == 0){
+    	    echo '<p style = "color: red;">';
+    	    echo 'You did not order anything from the previous page! </p>';
+    	    exit();
+    	}
+    	else {
+    	echo '<h2>Order Results</h2>';
+    	echo '<p>Order processed at ' . date('H:i, jS F Y') . "</p>";
+    	
+    	echo '<p style="font-weight: bold;">';
+    	switch ($find) {
+    	    case "a":
+                echo "Regular customer.";
+                break;
+    	    case "b":
+    	        echo "Customer reffered by TV advertisement.";
+    	        break;
+    	    case "c":
+    	        echo "Customer reffered by phone directory.";
+    	        break;
+    	    case "d":
+    	        echo "Customer reffered by word of mouth.";
+    	        break;
+    	    default:
+                echo "We do not know how this customer found us.";
+    	    break;
+    	}
+    	echo '</p>';
+    	
+    	echo '<p>Your order is as follows: </p>';
+    	
+    	if($tireqty > 0){
+    	echo htmlspecialchars($tireqty) . ' tires. <br/>';
+    	}
+    	if($oilqty > 0){
+    	echo htmlspecialchars($oilqty) . ' bottles of oil. <br/>';
+    	}
+    	if($sparkqty){
+    	echo htmlspecialchars($sparkqty) . ' spark plugs. <br/>';
+    	}
+    	
+    	echo "<p>";
+    	echo "Items ordered: $totalqty. <br/>";
 
-      $totalqty = 0;
-      $totalamount = 0.00;
-
-      define('TIREPRICE', 100);
-      define('OILPRICE', 10);
-      define('SPARKPRICE', 4);
-
-      $totalqty = $tireqty + $oilqty + $sparkqty;
-      echo "<p>Items ordered: ".$totalqty."<br />";
-
-      if ($totalqty == 0) {
-        echo "You did not order anything on the previous page!<br />";
-      } else {
-        if ($tireqty > 0) {
-          echo htmlspecialchars($tireqty).' tires<br />';
+    	$totalamount = $tireqty * TIREPRICE +
+    	               $oilqty * OILPRICE +
+    	               $sparkqty * SPARKPRICE;
+    	               
+    	echo "Subtotal: $" .number_format($totalamount, 2). "<br/>";
+    	
+    	$taxrate = 0.10; // local sales tax is 10%
+    	$totalamount = $totalamount * (1 + $taxrate);
+    	echo "Total including tax:  $" . number_format($totalamount, 2) . ".<br/>";
+    	
+    	echo "</p>";
+    	
+    	if($address != null){
+    	echo "Address to ship to is " . htmlspecialchars($address) . ".";
+    	}
+    	
+    	$outputstring = $date . "\t" . $tireqty . " tires \t" . $oilqty . " oil \t" . $sparkqty . " spark plugs \t \$" . $totalamount
+                        . "\t" . $address . "\n";
+    	
+        /** Codes from Chapter 2
+        // open file for appending
+        // $fp = fopen("$document_root/../orders/orders.txt", 'w+');
+        $fp = fopen("C:\\xampp\\htdocs\\phpandmysql\\Chapter02\\orders\\orders.txt", 'ab');
+        
+        if(!$fp){
+            echo "<p><strong> Your order could not be processed at this time. 
+                  Please try again later.</strong></p>";
+            exit();
         }
-        if ($oilqty > 0) {
-          echo htmlspecialchars($oilqty).' bottles of oil<br />';
-        }
-        if ($sparkqty > 0) {
-          echo htmlspecialchars($sparkqty).' spark plugs<br />';
-        }
-      }
-
-
-      $totalamount = $tireqty * TIREPRICE
-                   + $oilqty * OILPRICE
-                   + $sparkqty * SPARKPRICE;
-
-      echo "Subtotal: $".number_format($totalamount,2)."<br />";
-
-      $taxrate = 0.10;  // local sales tax is 10%
-      $totalamount = $totalamount * (1 + $taxrate);
-      echo "Total including tax: $".number_format($totalamount,2)."</p>";
-
-      echo "<p>Address to ship to is ".htmlspecialchars($address)."</p>";
-
-      $outputstring = $date."\t".$tireqty." tires \t".$oilqty." oil\t"
-                      .$sparkqty." spark plugs\t\$".$totalamount
-                      ."\t". $address."\n";
-
-      // open file for appending
-      try
-      {
-        if (!($fp = @fopen("$document_root/../orders/orders.txt", 'ab'))) {
-            throw new fileOpenException();
-        }
-      
-        if (!flock($fp, LOCK_EX)) {
-           throw new fileLockException();
-        }
-      
-        if (!fwrite($fp, $outputstring, strlen($outputstring))) {
-           throw new fileWriteException();
-        }
-
+        
+        flock($fp, LOCK_EX);
+        fwrite($fp, $outputstring, strlen($outputstring));
         flock($fp, LOCK_UN);
         fclose($fp);
-        echo "<p>Order written.</p>";
-      }
-      catch (fileOpenException $foe)
-      {
-         echo "<p><strong>Orders file could not be opened.<br/>
-               Please contact our webmaster for help.</strong></p>";
-      }
-      catch (Exception $e)
-      {
-         echo "<p><strong>Your order could not be processed at this time.<br/>
-               Please try again later.</strong></p>";
-      }
-    ?>
-  </body>
+        
+        echo '<p>Order written.</p>';
+        **/
+                        
+        try
+        {
+            if (!($fp = @fopen("C:\\xampp\\htdocs\\phpandmysql\\Chapter02\\orders\\orders.txt", 'ab'))) {
+                throw new fileOpenException();
+            }
+            
+            if (!flock($fp, LOCK_EX)) {
+                throw new fileLockException();
+            }
+            
+            if (!fwrite($fp, $outputstring, strlen($outputstring))) {
+                throw new fileWriteException();
+            }
+            
+            flock($fp, LOCK_UN);
+            fclose($fp);
+            echo "<p>Order written.</p>";
+        }
+        catch (fileOpenException $foe)
+        {
+            echo "<p><strong>Orders file could not be opened.<br/>
+Please contact our webmaster for help.</strong></p>";
+        }
+        catch (Exception $e)
+        {
+            echo "<p><strong>Your order could not be processed at this time.<br/>
+Please try again later.</strong></p>";
+        }
+    	}
+    	?>
+    </body>
 </html>
