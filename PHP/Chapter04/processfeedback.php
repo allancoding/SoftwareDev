d7ad7d3854b3f4ba1e0c14b9ba52ab3205b4155c
Allan Niles <allancoding.dev@gmail.com>
2025-03-31 09:28:44 -0600

    Update 4

--- PHP/Chapter04/processfeedback.php
+++ PHP/Chapter04/processfeedback.php
@@ -1,31 +1,32 @@
 <?php
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$feedback = trim($_POST['feedback']);
 
-$name=$_POST['name'];
-$email=$_POST['email'];
-$feedback=$_POST['feedback'];
-
+// setup some static information
 $toaddress = "feedback@example.com";
-
 $subject = "Feedback from web site";
-
-$mailcontent = "Customer name: ".filter_var($name)."\n".
-               "Customer email: ".$email."\n".
-               "Customer comments:\n".$feedback."\n";
-
+$mailcontent = "Customer name: " .str_replace("\r\n", "", $name). "\n" .
+                "Customer email: " .str_replace("\r\n", "", $email). "\n" .
+                "Customer comments: " .str_replace("\r\n", "", $feedback). "\n";
 $fromaddress = "From: webserver@example.com";
 
+// invoke mail() function to send mail
 mail($toaddress, $subject, $mailcontent, $fromaddress);
 
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Bob's Auto Parts - Feedback Submitted</title>
-  </head>
-  <body>
-
-    <h1>Feedback submitted</h1>
-    <p>Your feedback has been sent.</p>
-
-  </body>
+<head>
+	<title>Bob's Auto Parts - Feedback Submitted</title>
+</head>
+<body>
+	<h1>Feedback submitted</h1>
+	<p>Your feedback (shown below)  has been sent.</p>
+	<p>
+	<?php 
+	echo nl2br(htmlspecialchars($feedback));
+	?>
+	</p>
+</body>
 </html>
